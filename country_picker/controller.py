5f1376039cf167d1ce6cce3bcc7073c4534bf80a
DawidKrefta <dawidkre13@gmail.com>
2025-07-26 15:35:29 +0200

    Update label when country selected

--- country_picker/controller.py
+++ country_picker/controller.py
@@ -3,13 +3,24 @@ from .ui import MainWindow
 
 
 class AppController:
-    def __init__(self, main_window: MainWindow):
-        self.main_window = main_window
+    def __init__(self, window: MainWindow):
+        self.window = window
+        self._connect_signals()
         self._load_countries()
+
+    def _connect_signals(self):
+        self.window.combo_box.currentTextChanged.connect(
+            self._on_country_selected
+        )
+
+    def _on_country_selected(self, country_name: str):
+        self.window.label.setText(f"{country_name}")
 
     def _load_countries(self):
         try:
             countries = fetch_sorted_country_names()
-            self.main_window.combo_box.addItems(countries)
+            self.window.combo_box.addItems(countries)
+            self.window.combo_box.setCurrentIndex(-1)
+            self.window.label.setText("")
         except Exception:
-            self.main_window.label.setText("Failed to load countries.")
+            self.window.label.setText("Failed to load countries.")
